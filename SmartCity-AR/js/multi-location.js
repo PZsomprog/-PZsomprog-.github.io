f2d091243f9ce709e0b19c02e501b78bed7d4343
PZsomprog <zsombor.pancsics@gmail.com>
2021-02-25 23:14:01 +0100

    2021_25_23_13

--- SmartCity-AR/js/multi-location.js
+++ SmartCity-AR/js/multi-location.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                 lat: 47.31,
+                 lng: 19.7,
             },
         },
     ];
